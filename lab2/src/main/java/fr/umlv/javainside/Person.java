089570359d524ce3bdc53b5770c2ffc5c3495e35
cdu99 <calvin.du@vidal.fr>
2020-09-29 12:11:58 +0200

    lab2 JSON return

---
+++ lab2/src/main/java/fr/umlv/javainside/Person.java
@@ -0,0 +1,10 @@
+package fr.umlv.javainside;
+
+import static java.util.Objects.requireNonNull;
+
+public record Person(String firstName, String lastName) {
+    public Person {
+        requireNonNull(firstName);
+        requireNonNull(lastName);
+    }
+}
