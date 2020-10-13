869734c61422f7ec1f2ce3d5738b64d15badeabb
cdu99 <calvin.du@vidal.fr>
2020-10-13 14:46:59 +0200

    Question 8

--- lab4/src/main/java/fr/umlv/javainside/Logger.java
+++ lab4/src/main/java/fr/umlv/javainside/Logger.java
@@ -3,6 +3,7 @@ package fr.umlv.javainside;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.invoke.MutableCallSite;
 import java.lang.reflect.Method;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Objects;
@@ -30,6 +31,22 @@ public interface Logger {
         };
     }
 
+    public static Logger lambdaOf(Class<?> declaringClass, Consumer<? super String> consumer) {
+        Objects.requireNonNull(declaringClass);
+        Objects.requireNonNull(consumer);
+
+        var mh = createLoggingMethodHandle(declaringClass, consumer);
+        return (message) -> {
+            try {
+                mh.invokeExact(message);
+            } catch(RuntimeException | Error e) {
+                throw e;
+            } catch(Throwable t) {
+                throw new UndeclaredThrowableException(t);
+            }
+        };
+    }
+
     class Impl {
         // Pas de champs privé dans une interface donc on fait une classe
         private static final MethodHandle ACCEPT;
@@ -42,11 +59,32 @@ public interface Logger {
                 throw new AssertionError(e);
             }
         }
+
+        private static final ClassValue<MutableCallSite> ENABLE_CALLSITES = new ClassValue<>() {
+            protected MutableCallSite computeValue(Class<?> type) {
+                return new MutableCallSite(MethodHandles.constant(boolean.class, true));
+            }
+        };
+
+        public static void enable(Class<?> declaringClass, boolean enable) {
+            Impl.ENABLE_CALLSITES.get(declaringClass).setTarget(MethodHandles.constant(boolean.class, enable));
+        }
     }
 
+
+
     private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer){
-        var methodHandle = Impl.ACCEPT.bindTo(consumer);
-        methodHandle = methodHandle.asType(MethodType.methodType(void.class, String.class));
-        return methodHandle;
+//        var methodHandle = Impl.ACCEPT.bindTo(consumer);
+//        methodHandle = methodHandle.asType(MethodType.methodType(void.class, String.class));
+//        return methodHandle;
+
+        // Q8
+        var target = Impl.ACCEPT.bindTo(consumer);
+        target = target.asType(MethodType.methodType(void.class, String.class));
+        var test = Impl.ENABLE_CALLSITES.get(declaringClass).dynamicInvoker();
+        var fallback = MethodHandles.empty(MethodType.methodType(void.class, String.class));
+
+        return MethodHandles.guardWithTest(test, target, fallback);
+
     }
 }
