ab8c7061b89c94781b9468db2b1f9f2d1fc4e60c
cdu99 <calvin.du@vidal.fr>
2020-10-13 10:39:01 +0200

    Fix shit

--- lab4/src/main/java/fr/umlv/javainside/Logger.java
+++ lab4/src/main/java/fr/umlv/javainside/Logger.java
@@ -1,13 +1,20 @@
 package fr.umlv.javainside;
 
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public interface Logger {
     public void log(String message);
 
     public static Logger of(Class<?> declaringClass, Consumer<? super String> consumer) {
+        Objects.requireNonNull(declaringClass);
+        Objects.requireNonNull(consumer);
+
         var mh = createLoggingMethodHandle(declaringClass, consumer);
         return new Logger() {
             @Override
@@ -23,8 +30,23 @@ public interface Logger {
         };
     }
 
-    private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) {
-        // TODO
-        return null;
+    class Impl {
+        // Pas de champs privé dans une interface donc on fait une classe
+        private static final MethodHandle ACCEPT;
+        static {
+            var lookup = MethodHandles.lookup();
+            try {
+                ACCEPT = lookup.findVirtual(Consumer.class, "accept", MethodType.methodType(void.class, Object.class));
+            }
+            catch (NoSuchMethodException | IllegalAccessException e) {
+                throw new AssertionError(e);
+            }
+        }
+    }
+
+    private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer){
+        var methodHandle = Impl.ACCEPT.bindTo(consumer);
+        methodHandle = methodHandle.asType(MethodType.methodType(void.class, String.class));
+        return methodHandle;
     }
 }
